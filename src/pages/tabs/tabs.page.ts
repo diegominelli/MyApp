574c2879c21fc1ac0f97400859bfe27b97b44a60
diegominelli <diego.minelli@hotmail.com>
2020-06-29 17:17:52 -0300

    Inserção da page feed nas abas tabs.html

--- src/pages/tabs/tabs.page.ts
+++ src/pages/tabs/tabs.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Tab1Page } from '../tab1/tab1.page';
+import { Tab2Page } from '../tab2/tab2.page';
+import { Tab3Page } from '../tab3/tab3.page';
+import { FeedPage } from '../feed/feed.page';
 
 @Component({
   selector: 'app-tabs',
@@ -6,6 +10,11 @@ import { Component } from '@angular/core';
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
+
+  tab1Root = Tab1Page;
+  tab2Root = Tab2Page;
+  tab3Root = Tab3Page;
+  tab4Root = FeedPage;
 
   constructor() {}
 
